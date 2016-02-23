c3d0399719ee6063eae63adf4da4b94f792a27ba
Fabian <fabian.niederberger@stud.hslu.ch>
2016-02-23 21:09:04 +0100

    Open SDA and USB CDC Test Program
    With this program the OpenSDA USB interface is used for programing and
    debugging. The leftover KL25Z USB programing interface is modified to a
    serial port (USB CDC, appears as COM-port on device manager)
    The external clock on the board is used and modified to 48MHz
    
    The program echos everything you send it, for example:
    -> send: "Hello Freedom Board"
    -> receive: "echo: Hello Freedom Board"
    
    credits to Erich Styger and his MCUonEclipse Blog
    
    http://mcuoneclipse.com/2012/10/07/tutorial-usb-cdc-with-the-kl25z-freedom-board/

---
+++ FRDM_OpenSDA_USBCDC/Sources/main.c
@@ -0,0 +1,127 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : FRDM_OpenSDA_USBCDC
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-02-23, 19:12, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "LEDRed.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LEDGreen.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LEDBlue.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "WAIT1.h"
+#include "USB1.h"
+#include "USB0.h"
+#include "CDC1.h"
+#include "Tx1.h"
+#include "Rx1.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+//---------------------------
+//Global variables
+//---------------------------
+static uint8_t cdc_buffer[USB1_DATA_BUFF_SIZE];
+static uint8_t in_buffer[USB1_DATA_BUFF_SIZE];
+
+//---------------------------
+//Methods
+//---------------------------
+void CDC_Run(void){
+	int i;
+
+	for(;;){	//endless loop
+		while(CDC1_App_Task(cdc_buffer, sizeof(cdc_buffer))==ERR_BUSOFF){
+			// device not enumerated
+			LEDRed_Neg(); LEDGreen_Off();
+			WAIT1_Waitms(10);
+		}
+		LEDRed_Off(); LEDGreen_Neg();
+		if(CDC1_GetCharsInRxBuf() != 0){
+			i = 0;
+			while(i < sizeof(in_buffer)-1 && CDC1_GetChar(&in_buffer[i])==ERR_OK){
+				i++;
+			}
+			in_buffer[i] = '\0';
+			(void)CDC1_SendString((unsigned char*)"echo: ");
+			(void)CDC1_SendString(in_buffer);
+			(void)CDC1_SendString((unsigned char*)"\r\n");
+		}
+		else{
+			WAIT1_Waitms(10);
+		}
+	}
+}
+
+
+
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  CDC_Run();
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
